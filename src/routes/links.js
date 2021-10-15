0fc168eee1b21ac7111fb4c6059fa13c0fab325a
ApalmaP <andrepal14@gmail.com>
2021-10-14 23:51:02 -0500

    primera interfaz

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
+
+router.post('/add', (req, res) =>{
+    res.send('Recibido');   
+});
+
 module.exports = router;
